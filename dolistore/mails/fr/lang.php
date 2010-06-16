fd7d370accef2ebc56181aef9335bee1887ef79e
Cyrille de Lambert <cyrille.delambert@auguria.net>
2010-06-16 09:21:08 +0000

    Modification couleurs mail

---
+++ dolistore/mails/fr/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+global $_LANGMAIL;
+$_LANGMAIL = array();
+$_LANGMAIL['Your new admin password'] = 'Votre nouveau mot de passe administateur';
+$_LANGMAIL['Your password'] = 'Votre nouveau mot de passe';
+$_LANGMAIL['Welcome!'] = 'Bienvenue !';
+$_LANGMAIL['Order confirmation'] = 'Confirmation de commande';
+$_LANGMAIL['Message from contact form'] = 'Message depuis le formulaire de contact';
+$_LANGMAIL['My personal informations'] = 'Mes informations personnelles';
+$_LANGMAIL['Message from a customer'] = 'Message d\'un client';
+$_LANGMAIL['Virtual product to download'] = 'Produit virtuel à télécharger';
+$_LANGMAIL['Referral Program'] = 'Programme de parrainage';
+$_LANGMAIL['Package in transit'] = 'Colis en cours d\'envoie';
+
+?>
