631cf2a3a6fa101cfb8bf34e977cff591f88eb0c
Laurent Destailleur <eldy@destailleur.fr>
2012-10-08 02:39:04 +0200

    Work on democup presentation

---
+++ contests/owf_2012_democup/01_reinit_presentation.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+# Remove dolibarr debian installation
+echo
+echo STEP 1 - Removed Dolibarr
+sudo apt-get purge dolibarr
+
+# xxx
+echo
+echo STEP 2 - Wall paper
+echo Did you change your wallpaper with Dolibarr one ?
+echo
