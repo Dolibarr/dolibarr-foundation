6cc6fe82569b63cc960f2e0d3a378771f1d9ca4c
Laurent Destailleur <eldy@destailleur.fr>
2019-02-13 17:41:03 +0100

    Remove old files

--- dolistore/modules/old/blockmysales/fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{blockmysales}prestashop>blockmysales_4fbcb7e96a03e5df7f858a798208c7ea'] = 'Mes modules/produits';
-$_MODULE['<{blockmysales}prestashop>blockmysales_b1e3f7af9aa8a3f6ed794996acd5cb72'] = 'Soumettre un module/produit';
-$_MODULE['<{blockmysales}prestashop>blockmysales_152534fcdad1705727755f860b0b7419'] = 'Gérer mes modules/produits';
-$_MODULE['<{blockmysales}prestashop>blockmysales_56d2bca9bfd0a380822829cc7d71a587'] = 'Statistiques des ventes';
