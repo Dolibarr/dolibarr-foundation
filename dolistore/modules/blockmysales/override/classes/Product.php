f2f5053ce838778b41fde6e530eda0fab3000100
Regis Houssin <regis.houssin@inodbox.com>
2019-07-30 09:48:41 +0200

    FIX increase fields size in Product class override

--- dolistore/modules/blockmysales/override/classes/Product.php
+++ dolistore/modules/blockmysales/override/classes/Product.php
@@ -10,10 +10,10 @@ class Product extends ProductCore
 
 	public function __construct($id_product = null, $full = false, $id_lang = null, $id_shop = null, Context $context = null)
 	{
-		Product::$definition['fields']['module_version'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 7);
-		Product::$definition['fields']['dolibarr_min'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 5);
+		Product::$definition['fields']['module_version'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 15);
+		Product::$definition['fields']['dolibarr_min'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 6);
 		Product::$definition['fields']['dolibarr_min_status'] =  array('type' => self::TYPE_BOOL, 'validate' => 'isBool');
-		Product::$definition['fields']['dolibarr_max'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 5);
+		Product::$definition['fields']['dolibarr_max'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 6);
 		Product::$definition['fields']['dolibarr_max_status'] =  array('type' => self::TYPE_BOOL, 'validate' => 'isBool');
 		Product::$definition['fields']['dolibarr_core_include'] =  array('type' => self::TYPE_BOOL, 'validate' => 'isBool');
 
