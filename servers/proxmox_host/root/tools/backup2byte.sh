010302d0bc6f847a78ca001abd8253e3519102f9
Laurent Destailleur <eldy@destailleur.fr>
2019-10-30 19:54:26 +0100

    Clean deprecated files

--- servers/proxmox_host/root/tools/backup2byte.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-hostname='2byte.gotdns.com'
-username='dolibarr'
-
-rsync -a -vv --rsh 'ssh -p 65011' --delete -W --exclude .ssh --bwlimit=10000 /var/lib/vz/dump/. $username@$hostname:/media/COPIAS/dolibarr/
-
-echo End of rsync
-
--- servers/proxmox_host/root/tools/backup2nltechnofromrescue.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# To launch backup when server is launched in rescue mode
-hostname='ftp.nltechno.com'
-username='backup_proxmox'
-
-rsync -a -vv --delete-before -W --exclude .ssh --bwlimit=100000 /mnt/bbb/dump/*.lzo $username@$hostname:/home/backup_proxmox
-
-echo End of rsync
