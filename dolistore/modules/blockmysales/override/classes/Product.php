6fab30c8100964b661529edeeb4619a1c15d5ac4
Laurent Destailleur <eldy@destailleur.fr>
2021-01-06 14:59:30 +0100

    Fix override file

--- dolistore/modules/blockmysales/override/classes/Product.php
+++ dolistore/modules/blockmysales/override/classes/Product.php
@@ -3,15 +3,19 @@ class Product extends ProductCore
 {
 	public $module_version;
 	public $dolibarr_min;
+	//public $dolibarr_min_status;
 	public $dolibarr_max;
+	//public $dolibarr_max_status;
 	public $dolibarr_core_include;
 	public $dolibarr_disable_info;
 
 	public function __construct($id_product = null, $full = false, $id_lang = null, $id_shop = null, Context $context = null)
 	{
-		Product::$definition['fields']['module_version'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 4);
+		Product::$definition['fields']['module_version'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 15);
 		Product::$definition['fields']['dolibarr_min'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 6);
+		//Product::$definition['fields']['dolibarr_min_status'] =  array('type' => self::TYPE_BOOL, 'validate' => 'isBool');
 		Product::$definition['fields']['dolibarr_max'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 6);
+		//Product::$definition['fields']['dolibarr_max_status'] =  array('type' => self::TYPE_BOOL, 'validate' => 'isBool');
 		Product::$definition['fields']['dolibarr_core_include'] =  array('type' => self::TYPE_BOOL, 'validate' => 'isBool');
 		Product::$definition['fields']['dolibarr_disable_info'] =  array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 255);
 
