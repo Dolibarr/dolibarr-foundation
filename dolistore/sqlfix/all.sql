8ff578fee6b0e52e4057bb46bd1485ec3ce40cde
Laurent Destailleur <eldy@destailleur.fr>
2023-09-26 12:12:17 +0200

    Add SQL to remove from Home, all modules not compatible with v18

--- dolistore/sqlfix/all.sql
+++ dolistore/sqlfix/all.sql
@@ -8,3 +8,23 @@ alter table ps_product ADD COLUMN dolibarr_max varchar(6);
 alter table ps_product ADD COLUMN dolibarr_max_status int(1);
 alter table ps_product ADD COLUMN dolibarr_core_include int(1);
 alter table ps_product ADD COLUMN dolibarr_disable_info varchar(255);
+
+
+-- Remove the Home category for modules that are lower than a given version
+-- To find modules:
+SELECT cp.id_category, cp.id_product from ps_product as p, ps_category_product as cp 
+WHERE cp.id_category=1 AND p.id_product = cp.id_product AND (p.dolibarr_max IS NULL OR p.dolibarr_max < 18.0);
+
+-- To select 
+SELECT * FROM ps_category_product as T WHERE EXISTS (
+SELECT 1 from ps_product as p, ps_category_product as cp 
+WHERE cp.id_category=1 AND p.id_product = cp.id_product AND (p.dolibarr_max IS NULL OR p.dolibarr_max < 18.0)
+AND cp.id_product = T.id_product AND cp.id_category = T.id_category
+)
+
+-- To delete
+--DELETE FROM ps_category_product as T WHERE EXISTS (
+--SELECT 1 from ps_product as p, ps_category_product as cp 
+--WHERE cp.id_category=1 AND p.id_product = cp.id_product AND (p.dolibarr_max IS NULL OR p.dolibarr_max < 18.0)
+--AND cp.id_product = T.id_product AND cp.id_category = T.id_category
+--)
