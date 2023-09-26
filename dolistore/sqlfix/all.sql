65836bc8102c020b7cbd69b8f3510df8e201be76
Laurent Destailleur <eldy@destailleur.fr>
2023-09-26 22:45:46 +0200

    Clean SQL

--- dolistore/sqlfix/all.sql
+++ dolistore/sqlfix/all.sql
@@ -20,8 +20,13 @@ AND cp.id_product = T.id_product AND cp.id_category = T.id_category
 )
 
 -- To delete
---DELETE FROM ps_category_product as T WHERE EXISTS (
---SELECT 1 from ps_product as p, ps_category_product as cp 
---WHERE cp.id_category=1 AND p.id_product = cp.id_product AND (p.dolibarr_max IS NULL OR p.dolibarr_max < 18.0)
---AND cp.id_product = T.id_product AND cp.id_category = T.id_category
---)
+--CREATE TABLE temp_aaa AS (SELECT p.id_product from ps_product as p, ps_category_product as cp WHERE cp.id_category=1 AND p.id_product = cp.id_product AND (p.dolibarr_max IS NULL OR p.dolibarr_max < 18.0));
+--DELETE FROM ps_category_product WHERE id_category = 1 AND id_product IN (SELECT id_product from temp_aaa);
+--DROP TABLE temp_aaa;
+
+
+-- Remove module from the category Goodies
+
+--CREATE TABLE temp_bbb AS (SELECT cp.id_product from ps_category_product as cp WHERE cp.id_category = (select distinct id_category from ps_category_lang where name = 'Goodies'));
+--DELETE FROM ps_category_product WHERE id_product IN (SELECT id_product from temp_bbb) AND id_category = (select distinct id_category from ps_category_lang where name = 'Goodies');
+--DROP TABLE temp_bbb;
