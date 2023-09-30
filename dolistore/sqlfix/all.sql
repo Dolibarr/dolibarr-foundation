bb2033a23074c59ae28aad83624d9f9d28b7f8c5
Laurent Destailleur <eldy@destailleur.fr>
2023-09-30 19:28:22 +0200

    Add request to count number of modules on Home page per publisher

--- dolistore/sqlfix/all.sql
+++ dolistore/sqlfix/all.sql
@@ -19,14 +19,25 @@ WHERE cp.id_category=1 AND p.id_product = cp.id_product AND (p.dolibarr_max IS N
 AND cp.id_product = T.id_product AND cp.id_category = T.id_category
 )
 
--- To delete
+-- To remove modules from category Home when their version is lower than X.y  
+--DROP TABLE temp_aaa;
 --CREATE TABLE temp_aaa AS (SELECT p.id_product from ps_product as p, ps_category_product as cp WHERE cp.id_category=1 AND p.id_product = cp.id_product AND (p.dolibarr_max IS NULL OR p.dolibarr_max < 18.0));
 --DELETE FROM ps_category_product WHERE id_category = 1 AND id_product IN (SELECT id_product from temp_aaa);
---DROP TABLE temp_aaa;
 
 
--- Remove module from the category Goodies
-
+-- To remove modules from the category Goodies
+--DROP TABLE temp_bbb;
 --CREATE TABLE temp_bbb AS (SELECT cp.id_product from ps_category_product as cp WHERE cp.id_category = (select distinct id_category from ps_category_lang where name = 'Goodies'));
 --DELETE FROM ps_category_product WHERE id_product IN (SELECT id_product from temp_bbb) AND id_category = (select distinct id_category from ps_category_lang where name = 'Goodies');
---DROP TABLE temp_bbb;
+
+
+-- To remove bad strings into description lines
+UPDATE ps_product_lang SET description = REPLACE(description, 'abc', 'def') WHERE description LIKE '%abc%';
+
+
+-- To count number of modules for a publisher from category Home  
+SELECT SUBSTR(p.reference, 2, POSITION('d' IN p.reference) -2), c.email, c.company, COUNT(p.id_product) from ps_product as p, ps_customer as c 
+WHERE p.active = 1 AND p.id_product IN (SELECT cp.id_product FROM ps_category_product as cp WHERE cp.id_category=1)
+AND c.id_customer = SUBSTR(p.reference, 2, POSITION('d' IN p.reference) -2)
+GROUP BY SUBSTR(p.reference, 2, POSITION('d' IN p.reference) -2)
+
