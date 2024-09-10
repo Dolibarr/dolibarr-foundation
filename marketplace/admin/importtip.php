6178cfda7f3c78084095029b60419949146d675c
Mohamed DAOUD <daoud.mouhamed@gmail.com>
2024-09-10 20:14:28 +0200

    Update importtip.php

--- marketplace/admin/importtip.php
+++ marketplace/admin/importtip.php
@@ -46,9 +46,6 @@ if (!$res && file_exists("../../main.inc.php")) {
 if (!$res && file_exists("../../../main.inc.php")) {
 	$res = @include "../../../main.inc.php";
 }
-if (!$res && file_exists("../../../../main.inc.php")) {
-    $res = @include "../../../../main.inc.php";
-}
 if (!$res) {
 	die("Include of main fails");
 }
