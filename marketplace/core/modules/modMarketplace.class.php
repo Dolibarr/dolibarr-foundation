4ece6e17259ddcd9376d2bcd77462004058435d6
ldestailleur <eldy@destailleur.fr>
2025-03-28 13:24:40 +0100

    Title email

--- marketplace/core/modules/modMarketplace.class.php
+++ marketplace/core/modules/modMarketplace.class.php
@@ -762,7 +762,7 @@ class modMarketplace extends DolibarrModules
 			<p>__(emailTemplateRegards)__</p>
 			<p>__(emailTemplateTeam)__</p>';
 
-			$email_sql = "INSERT INTO ".MAIN_DB_PREFIX."c_email_templates (label, lang, module, type_template, fk_user, private, position, topic, email_from, joinfiles, defaultfortype, content, entity, active, enabled) VALUES ('(resetPasswordMarketplace)', '', 'marketplace', 'thirdparty', null, 0, 120, '[__[MARKETPLACE_NAME]__] __(passwordResetRequest)__ (__USER_REMOTE_IP__)', null, 0, 0, '" . $email_content . "' , ".((int) $conf->entity).", 1, 1);";
+			$email_sql = "INSERT INTO ".MAIN_DB_PREFIX."c_email_templates (label, lang, module, type_template, fk_user, private, position, topic, email_from, joinfiles, defaultfortype, content, entity, active, enabled) VALUES ('(resetPasswordMarketplace)', '', 'marketplace', 'thirdparty', null, 0, 120, '[__[MARKETPLACE_NAME]__] __(passwordResetRequest)__ (IP __USER_REMOTE_IP__)', null, 0, 0, '" . $email_content . "' , ".((int) $conf->entity).", 1, 1);";
 			$result = $this->db->query($email_sql);
 			if ($result) {
 				$id_template = $this->db->last_insert_id(MAIN_DB_PREFIX."c_email_templates");
