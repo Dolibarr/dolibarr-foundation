8fecef7668bf0cb27f7fe3f90c3155192c0bbd2a
Mohamed DAOUD <daoud.mouhamed@gmail.com>
2025-05-27 16:53:08 +0200

    fix js error

--- marketplace/doctemplates/websites/website_template-dolistore/containers/styles.css.php
+++ marketplace/doctemplates/websites/website_template-dolistore/containers/styles.css.php
@@ -4611,8 +4611,8 @@ div.radio {
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
-  background-color: rgba(0,0,0,.7)!important
+  /*height: 100%;*/
+  /*background-color: rgba(0,0,0,.7)!important*/
 }
 .countdown-container,
 .lightbox3 #content3,
