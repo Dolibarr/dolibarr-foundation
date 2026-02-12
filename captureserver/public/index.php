2a1366202bc7844ea76b43cdd33fac944b4bf464
Laurent Destailleur <eldy@destailleur.fr>
2026-02-12 23:29:31 +0100

    Error

--- captureserver/public/index.php
+++ captureserver/public/index.php
@@ -84,7 +84,7 @@ if (! $action) {
 // Complete urls for post treatment
 $SECUREKEY = GETPOST("securekey", 'alpha');	        // Secure key
 if ($SECUREKEY && $SECUREKEY != getDolGlobalString("CAPTURESERVER_SECURITY_KEY")) {
-	dol_syslog('ERROR ErrorBadParameters - action missing', LOG_WARNING, 0, '_captureserver');
+	dol_syslog('ERROR ErrorBadSecureKey - Bad value of securekey parameter', LOG_WARNING, 0, '_captureserver');
 	accessforbidden("ERROR Acces not allowed. Bad value of securekey parameter");
 }
 
