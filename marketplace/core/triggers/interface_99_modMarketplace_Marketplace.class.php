b260f58ff40108bf2b15d9110c3fb73c49144615
Laurent Destailleur <eldy@destailleur.fr>
2026-03-24 16:36:30 +0100

    Doc

--- marketplace/core/triggers/interface_99_modMarketplace_Marketplace.class.php
+++ marketplace/core/triggers/interface_99_modMarketplace_Marketplace.class.php
@@ -103,8 +103,9 @@ class InterfaceMarketplace extends DolibarrTriggers
 				 var_dump($object->lines);*/
 
 				if (isset($object->oldcopy)) {
-					dol_syslog("We check that we don't try to enabled a product or service if ForkOf or LawViolation is on");
-
+					dol_syslog("We check that we don't try to enable a product or service if ForkOf or LawViolation or DateStopEarning is on");
+					dol_syslog(json_encode($object->oldcopy));
+					dol_syslog(json_encode($object->oldcopy->array_options));
 					dol_syslog("olddata: ".$object->oldcopy->array_options['options_marketplace_law_violation']." ".$object->oldcopy->array_options['options_marketplace_fork_of']." ".$object->oldcopy->array_options['options_marketplace_date_stop_earning']);
 					dol_syslog("data: ".$object->array_options['options_marketplace_law_violation']." ".$object->array_options['options_marketplace_fork_of']." ".$object->array_options['options_marketplace_date_stop_earning']);
 
