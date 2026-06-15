274a304a4fe80946cac8242007dfa292b051ddf4
Laurent Destailleur <eldy@destailleur.fr>
2026-06-15 18:27:40 +0200

    Removed const

--- captureserver/core/modules/modCaptureServer.class.php
+++ captureserver/core/modules/modCaptureServer.class.php
@@ -125,7 +125,7 @@ class modCaptureServer extends DolibarrModules
 		//                             1=>array('CAPTURESERVER_MYNEWCONST2','chaine','myvalue','This is another constant to add',0, 'current', 1)
 		// );
 		$this->const = array(
-			1=>array('CAPTURESERVER_MYCONSTANT', 'chaine', 'avalue', 'This is a constant to add', 1, 'allentities', 1)
+			//1=>array('CAPTURESERVER_MYCONSTANT', 'chaine', 'avalue', 'This is a constant to add', 1, 'allentities', 1)
 		);
 
 		// Some keys to add into the overwriting translation tables
