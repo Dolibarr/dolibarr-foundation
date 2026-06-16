c21cf12a955919769c5dbea98d1a929a590c93ba
Laurent Destailleur <eldy@destailleur.fr>
2026-06-17 01:09:35 +0200

    Merge branch 'develop' of git@github.com:/Dolibarr/dolibarr-foundation into develop

--- captureserver/public/index.php
+++ captureserver/public/index.php
@@ -150,6 +150,7 @@ if ($action == 'dolibarrping'
 			if ($action == 'dolibarrgetkeyobfuscation') {
 				$tmparray = json_decode($contenttoinsert, true, 2);
 
+				/*
 				if ($tmparray['company_idprof1'] != $captureserver->registerprofid) {
 					$message = 'The professional ID received ('.$tmparray['company_idprof1'].') is not the same than the one registered ('.$captureserver->registerprofid.') for registration number '.$hash_unique_id.' or was never registered. Go to the setup page of module BlockedLog to register your instance.';
 					dol_syslog($message, LOG_DEBUG, 0, '_captureserver');
@@ -159,9 +160,10 @@ if ($action == 'dolibarrping'
 					$db->close();
 					exit;
 				}
+				*/
 
 				// Return the obfuscation key
-				print 'dolobfuscationv1-'.$tmparray['company_idprof1'].':DOLOBFUSCKEYV1'.hash('sha256', $tmparray['company_idprof1'].getDolGlobalString('CAPTURESERVER_SALT_FOR_OBFUSCATIONKEY'))."\n";
+				print 'DOLOBFUSCKEYV1'.hash('sha256', $tmparray['company_idprof1'].getDolGlobalString('CAPTURESERVER_SALT_FOR_OBFUSCATIONKEY'))."\n";
 
 				exit;
 			}
