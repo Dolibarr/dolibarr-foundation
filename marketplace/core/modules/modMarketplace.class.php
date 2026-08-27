708bca0a4fcd44cb07f1b5165289fd3b93dc0d05
Laurent Destailleur <eldy@destailleur.fr>
2026-08-27 15:36:31 +0200

    Fix typocss

--- marketplace/core/modules/modMarketplace.class.php
+++ marketplace/core/modules/modMarketplace.class.php
@@ -518,7 +518,7 @@ class modMarketplace extends DolibarrModules
 		$result17=$extrafields->addExtraField('marketplace_duplicate_of', "DuplicateOf", 'link', 110155, '', 'product', 0, 0, '', $param, 1, '', 1, 'WhenModuleIsACopyOfOriginal', '', '', 'marketplace@marketplace', 'isModEnabled("marketplace")', 0, 0, array('csslist'=>'tdoverflowmax100'));
 		$result18=$extrafields->addExtraField('marketplace_fork_of', "ForkOf", 'link', 110160, '', 'product', 0, 0, '', $param, 1, '', 1, 'WhenModuleIsAForkOfAnother', '', '', 'marketplace@marketplace', 'isModEnabled("marketplace")', 0, 0, array('csslist'=>'tdoverflowmax100'));
 		$result19=$extrafields->addExtraField('marketplace_law_violation', "LawViolation", 'varchar', 110165, '255', 'product', 0, 0, '', $param, 1, '', 1, 'WhenModuleViolatesRules', '', '', 'marketplace@marketplace', 'isModEnabled("marketplace")', 0, 0, array('csslist'=>'tdoverflowmax100'));
-		$result20=$extrafields->addExtraField('marketplace_date_stop_earning', "DateStopEarning", 'datetime', 110167, '255', 'product', 0, 0, '', $param, 1, '', 1, 'ToStopAllEarningAfterAGivenDate', '', '', 'marketplace@marketplace', 'isModEnabled("marketplace")', 0, 0, array('csslist'=>'tdoverflowmax100'));
+		$result20=$extrafields->addExtraField('marketplace_date_stop_earning', "DateStopEarning", 'datetime', 110167, '255', 'product', 0, 0, '', $param, 1, '', 1, 'ToStopAllEarningAfterAGivenDate', '', '', 'marketplace@marketplace', 'isModEnabled("marketplace")', 0, 0);
 
 
 		// Permissions
